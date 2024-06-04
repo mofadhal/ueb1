cddeafd4f0c1b7f8fedee598881b16f250cdfbed
Mofadhal Al-Manari <al.manari@hotmail.com>
2024-06-05 00:12:51 +0200

    Last push

---
+++ Random.cpp
@@ -0,0 +1,20 @@
+/**
+*@author  Mofadhal Al-Manari and Mohamad Ata Suleiman and Clarita Kawam (Gruppe 22)
+ *@version  15.05.2024
+ **/
+
+#include "Random.h"
+#include <cstdlib> // For std::rand, std::srand
+#include <ctime>   // For std::time
+
+void initGenerator() {
+    std::srand(time(0));
+}
+
+int erzeugeInt(int maxWert) {
+    if (maxWert > 0) {
+        return std::rand() % (maxWert + 1);
+    } else {
+        return std::rand();
+    }
+}
